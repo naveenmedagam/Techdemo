e389603ab29e4ca7c7b3b286c50e71ae90c85dae
Naveen Medagam <naveen.medagam@gmail.com>
2021-01-06 10:43:53 -0600

    adding cross origin

--- src/main/java/com/aws/techdemo/restcontroller/DemoController.java
+++ src/main/java/com/aws/techdemo/restcontroller/DemoController.java
@@ -2,6 +2,7 @@ package com.aws.techdemo.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,14 @@ public class DemoController {
 	@Autowired
 	private PropertyService propertyService;
 
+	@CrossOrigin
 	@GetMapping(value = "/property/list")
 	public PropertyDataResponse listProperties(@RequestParam(required = false) String districtName,Pageable pageable) {
 
 		return propertyService.getAllProperties(pageable, districtName);
 	}
 
+	 @CrossOrigin
 	 @GetMapping(value = "/property/{id}") 
 	 public PropertyDetail getpropertyById(@PathVariable  int id) throws NotFoundException{
 	  
